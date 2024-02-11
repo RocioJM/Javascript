cbd93686cd1007708a594be9ca8e35d84bfbae46
Rocio Jimenez <rociojimenezmoreno@gmail.com>
2024-02-11 15:20:57 +0100

    Version del profesor añadida.

--- JohnMircha/EjerciciosLogicaProgramacion/Ejercicios8/index.js
+++ JohnMircha/EjerciciosLogicaProgramacion/Ejercicios8/index.js
@@ -51,7 +51,27 @@
 
 //Version2
 {
+    const ordenarArreglo = (arr = undefined) =>{
+        if(arr === undefined) return console.warn("No ingesaste un arreglo de numeros.");
+        if(!(arr instanceof Array)) return console.error("El valor que ingresastes no es un arreglo.");
+        if(arr.length===0) return console.error("El arreglo esta vacio.");
 
+        for(let num of arr)
+        {
+            if(typeof num!=="number") return console.error(`El valor "${num}" ingresado, NO es un numero`);
+        }
+
+        return console.info({
+            arr,
+            asc: arr.map(el => el).sort(),
+            desc: arr.map(el => el).sort().reverse()
+        });
+    }
+    ordenarArreglo();
+    ordenarArreglo("Hola");
+    ordenarArreglo([]);
+    ordenarArreglo([3,{}]);
+    ordenarArreglo([7, 5,7,8,6]);
 }
 
 //25) Programa una función que dado un arreglo de elementos, elimine los duplicados, pe. miFuncion(["x", 10, "x", 2, "10", 10, true, true]) devolverá ["x", 10, 2, "10", true].
@@ -92,7 +112,17 @@
 
 //Version2
 {
-    
+    const quitarDuplicados = (arr = undefined) =>{
+        if(arr === undefined) return console.warn("No ingesaste un arreglo de numeros.");
+        if(!(arr instanceof Array)) return console.error("El valor que ingresastes no es un arreglo.");
+        if(arr.length===0) return console.error("El arreglo esta vacio.");
+
+        return console.info({
+            original: arr,
+            sinDuplicados: arr.filter((value,index,self) => self.indexOf(value)===index)
+        });
+    }
+    quitarDuplicados(["x", 10, "x", 2, "10", 10, true, true]);
 }
 
 //26) Programa una función que dado un arreglo de números obtenga el promedio, pe. promedio([9,8,7,6,5,4,3,2,1,0]) devolverá 4.5.
@@ -117,5 +147,27 @@
 
 //Version2
 {
+    const NuevoPromedio = (arr = undefined) =>{
+        if(arr === undefined) return console.warn("No ingesaste un arreglo de numeros.");
+        if(!(arr instanceof Array)) return console.error("El valor que ingresastes no es un arreglo.");
+        if(arr.length===0) return console.error("El arreglo esta vacio.");
 
+        for(let num of arr)
+        {
+            if(typeof num!=="number") return console.error(`El valor "${num}" ingresado, NO es un numero`);
+        }
+
+        return console.info(
+            arr.reduce((total,num, index,arr) =>{
+                total+= num;
+                if(index===arr.length-1)
+                {
+                    return `El promedio de ${arr.join(" + ")} es ${total/arr.length}`;
+                }else{
+                    return total;
+                }
+            })
+        );
+    }
+    NuevoPromedio([9,8,7,6,5,4,3,2,1,0]);
 }
